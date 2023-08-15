639f5fe7538e3e6ce9a9d2958ef20fb23833b739
Surafel Tad <surafelbekele978@gmail.com>
2023-08-15 08:50:17 -0700

    firstt commit

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ const App = () => {
   <>
   <nav>
     <ul>
+    
         <li className="nav-todo"> <Link to="/SignUp"> <HowToRegIcon from/> SignUp </Link></li>
         <li className="nav-todo"> <Link to="/moviee"> <MovieFilterIcon/> Movie </Link> </li>
         <li className="nav-todo"> <Link to="/"> <LoginIcon/> Login </Link> </li>
