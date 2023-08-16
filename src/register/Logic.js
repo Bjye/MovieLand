356b6c2da532b81f80a741230b7f504f781ffb33
Surafel Tad <surafelbekele978@gmail.com>
2023-08-16 10:44:30 -0700

    modified

--- src/register/Logic.js
+++ src/register/Logic.js
@@ -1,3 +1,56 @@
+// import {useState, useEffect} from "react";
+// import "../App.css";
+// import SearchIcon from "../search.svg";
+// import MovieCard from "../MovieCard";
+
+
+// const API_URL = "http://www.omdbapi.com?apikey=4dc9bf8a";
+
+
+// const Logic = () => {
+//     const [movies, setmovies] = useState([]);
+//     const [searchTerm, setSearchTerm] = useState('');
+
+//     const searchMovies = async (title) =>{
+//         const response = await fetch(`${API_URL}&s=${title}`);
+//         const data = await response.json();
+//         setmovies(data.Search);
+//     } 
+//  useEffect(()=>{
+//     searchMovies('The Amazing spider man')  
+//  }, [])
+
+//     return(
+//         <div className="app">
+//           <h1>MovieLand</h1>
+//           <div className="search">
+//         <input placeholder="Search for movies"
+//             value= {searchTerm}
+//             onChange={(e)=> setSearchTerm(e.target.value)}
+//         />
+
+//       <img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
+//       </div>
+    
+//        {
+//         movies.length > 0 
+//           ? ( <div className="container">
+//                 {movies.map((movie) => (<MovieCard movie = {movie}/>)) }
+//             </div>  
+//             ) :
+//             (
+//               <div className="empty">
+//                 <h2>No movies found</h2>
+//               </div>
+//             )
+//        }
+        
+//       </div> 
+//     );
+// }
+// export default Logic;
+
+
 import {useState, useEffect} from "react";
 import "../App.css";
 import SearchIcon from "../search.svg";
@@ -5,17 +58,32 @@ import MovieCard from "../MovieCard";
 
 
 const API_URL = "http://www.omdbapi.com?apikey=4dc9bf8a";
-
+// const API_URL = "http://www.omdbapi.com/?apikey=4dc9bf8a";
 
 const Logic = () => {
     const [movies, setmovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
 
-    const searchMovies = async (title) =>{
+    // const searchMovies = async (title) =>{
+    //     const response = await fetch(`${API_URL}&s=${title}`);
+    //     const data = await response.json();
+    //     setmovies(data.Search);
+    // } 
+
+    const searchMovies = async (title) => {
+      try {
         const response = await fetch(`${API_URL}&s=${title}`);
+        if (!response.ok) {
+          throw new Error("Failed to fetch movies");
+        }
         const data = await response.json();
         setmovies(data.Search);
-    } 
+      } catch (error) {
+        console.error(error);
+        // Perform error handling, such as displaying an error message to the user
+      }
+    };
+
  useEffect(()=>{
     searchMovies('The Amazing spider man')  
  }, [])
--- src/register/Login.js
+++ src/register/Login.js
@@ -1,28 +1,99 @@
+// import React, { useState } from "react";
+// import "./Style.css";
+
+// export default function Login(props){
+//     const [email, setEmail] = useState('');
+//     const [pass, setPass] = useState('');
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         console.log(email);
+//     }
+
+//     return (
+//         <div className="Appp">
+//         <div className="auth-form-container  login-form">
+//             <h2>Login</h2>
+//             <form className="login-form" onSubmit={handleSubmit}>
+//                 <label className="r-label" htmlFor="email">email</label>
+//                 <input className="r-input" value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+//                 <label className="r-label" htmlFor="password">password</label>
+//                 <input className="r-input" value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+//                 <button className="button-sign" type="submit">Log In</button>
+//             </form>
+//             <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+//         </div>
+//         </div>
+//     )
+// }
+
+
 import React, { useState } from "react";
 import "./Style.css";
+import Logic from "./Logic";
 
-export default function Login(props){
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
+export default function Login(props) {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(email);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Check if the entered email and password match the fixed values
+    if (email === "auwra@gmail.com" && pass === "123456") {
+      console.log("Login successful");
+      setLoggedIn(true);
+    } else {
+      console.log("Invalid email or password");
+      // Display an error message or perform any other error handling
     }
+  };
 
-    return (
-        <div className="Appp">
-        <div className="auth-form-container  login-form">
-            <h2>Login</h2>
-            <form className="login-form" onSubmit={handleSubmit}>
-                <label className="r-label" htmlFor="email">email</label>
-                <input className="r-input" value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-                <label className="r-label" htmlFor="password">password</label>
-                <input className="r-input" value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-                <button className="button-sign" type="submit">Log In</button>
-            </form>
-            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
-        </div>
-        </div>
-    )
+  if (loggedIn) {
+    return <Logic />;
+  }
+
+  return (
+    <div className="Appp">
+      <div className="auth-form-container login-form">
+        <h2>Login</h2>
+        <form className="login-form" onSubmit={handleSubmit}>
+          <label className="r-label" htmlFor="email">
+            email
+          </label>
+          <input
+            className="r-input"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            placeholder="youremail@gmail.com"
+            id="email"
+            name="email"
+          />
+          <label className="r-label" htmlFor="password">
+            password
+          </label>
+          <input
+            className="r-input"
+            value={pass}
+            onChange={(e) => setPass(e.target.value)}
+            type="password"
+            placeholder="********"
+            id="password"
+            name="password"
+          />
+          <button className="button-sign" type="submit">
+            Log In
+          </button>
+        </form>
+        <button
+          className="link-btn"
+          onClick={() => props.onFormSwitch("register")}
+        >
+        <a href="/SignUp" className="link-btn"> Don't have an account? Register here.</a>
+        </button>
+      </div>
+    </div>
+  );
 }
